1fb45ba036197d2b27235774646fc2df7cce46fa
Luc-Frédéric Langis <llangis@splio.com>
2019-07-09 13:59:42 +0200

    feat(player): should let flash the player in the current state

---
+++ characters/enemies/HitBox.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+
+func _ready():
+	self.connect('body_entered', self, '_on_Body_enter')
+
+ 
+func _on_Body_enter(player: Player) -> void:
+	player.get_node('Health').take_damage(25)
