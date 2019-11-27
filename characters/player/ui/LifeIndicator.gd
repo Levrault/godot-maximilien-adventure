e1ba91950dd4bdb8d536280360f661733b916780
levrault <lucf.langis@gmail.com>
2019-11-27 16:45:07 +0100

    feat(life): change life player behavior

--- characters/player/ui/LifeIndicator.gd
+++ characters/player/ui/LifeIndicator.gd
@@ -1,9 +1,22 @@
 extends HBoxContainer
+var Icon = preload('res://characters/player/ui/PlayerIcon.tscn')
 
 
 func _ready() -> void:
 	GameManager.connect('player_life', self, '_on_Player_life_change')
+	_on_Player_life_change(GameManager.player_life)
 
 
 func _on_Player_life_change(life: int) -> void:
-	$Label.text = '%s' % life
+	# clear previous icon
+	for icon in get_children():
+		icon.queue_free()
+	
+	# new values
+	var i := 0
+	while(i < life):
+		var icon = Icon.instance()
+		icon.rect_size = Vector2(12, 12)
+		icon.rect_min_size = Vector2(12, 12)
+		add_child(icon)
+		i+=1
