bf866f558cdd07faa2a1ab0170a0fe6fa1384e72
levrault <lucf.langis@gmail.com>
2020-01-21 16:25:56 +0100

    feat(environment): export bush to his own tilemap

--- props/cart/RailsCart.gd
+++ props/cart/RailsCart.gd
@@ -6,14 +6,20 @@ extends Node2D
 
 export (int) var length := 96
 export (String) var next_level := ""
+export (bool) var has_cart := true
 
-const exist_dist := 50
+const EXIST_DIST := 50
 
 
 func _ready():
 	$Rails.rect_size = Vector2(length, 32)
-	$ExitZone.position.x = (length - exist_dist)
-	$ExitZone.next_level = next_level
+	
+	if not has_cart:
+		$Cart.queue_free()
+		$ExitZone.queue_free()
+	else:
+		$ExitZone.position.x = (length - EXIST_DIST)
+		$ExitZone.next_level = next_level
 
 
 """
@@ -23,4 +29,7 @@ func _process(delta: float):
 	if Engine.editor_hint:
 		if ($Rails.rect_size.x != length):
 			$Rails.rect_size = Vector2(length, 32)
-			$ExitZone.position.x = (length - exist_dist)
+			$ExitZone.position.x = (length - EXIST_DIST)
+		if not has_cart:
+			$ExitZone.hide()
+			$Cart.hide()
