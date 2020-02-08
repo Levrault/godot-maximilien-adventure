99a5aa1fe1ae8a5d8fcc7a989fb3e100cbc346da
levrault <lucf.langis@gmail.com>
2020-02-08 11:34:51 +0100

    tool(screenshot): add a screenshot tools

--- characters/player/ui/PlayerUI.gd
+++ characters/player/ui/PlayerUI.gd
@@ -2,8 +2,8 @@ extends CanvasLayer
 
 
 func _ready():
-	UiManager.connect("player_ui_hide", self, "_on_Hide")
-	UiManager.connect("player_ui_show", self, "_on_Show")
+	UiManager.connect("ui_player_hide", self, "_on_Hide")
+	UiManager.connect("ui_player_show", self, "_on_Show")
 	DialogueManager.connect("start_dialogue", self, "_on_Hide")
 	DialogueManager.connect("end_dialogue", self, "_on_Show")
 
@@ -11,7 +11,7 @@ func _ready():
 """
 Hide all content
 
-@signal player_ui_hide
+@signal ui_player_hide
 @signal start_dialogue
 """
 func _on_Hide() -> void:
@@ -22,7 +22,7 @@ func _on_Hide() -> void:
 """
 Hide all content
 
-@signal player_ui_show
+@signal ui_player_show
 @signal end_dialogue
 """
 func _on_Show() -> void:
