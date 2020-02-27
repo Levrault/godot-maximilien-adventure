8a83c53a93ce36d7e8827dee4191867b0b5acc63
levrault <lucf.langis@gmail.com>
2020-02-27 11:28:56 +0100

    fix(interface): prevent a issue where you couldn't load a save after returning to menu

--- interfaces/menu/Profile.gd
+++ interfaces/menu/Profile.gd
@@ -22,8 +22,10 @@ Show profile options
 func _on_Button_pressed() -> void:
 	if is_new_profile:
 		ProgressionManager.path = profile_save
+		get_tree().get_root().get_node("Menu").queue_free()
 		get_tree().change_scene("res://interfaces/menu/ProfileCreator.tscn")
 	else:
+		print(get_name())
 		emit_signal("profile_selected", $Parchment/PixelButton.text, profile_save, get_name())
 
 
