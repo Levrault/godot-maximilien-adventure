83657c676ebb240d912cfc01b8fd83076d59eec5
levrault <lucf.langis@gmail.com>
2020-03-20 14:05:41 +0100

    feat(level): fog should indicate level limit

---
+++ environment/fog/Fog.gd
@@ -0,0 +1,39 @@
+extends Sprite
+
+const VALUE := 3.0
+const DURATION := 2.0
+enum { FORWARD, BACKWARD }
+var index = FORWARD
+var from = 0
+var to = 0
+
+
+func _ready() -> void:
+	$Tween.connect("tween_completed", self, "_on_Tween_completed")
+	from = position
+	to = Vector2(position.x + VALUE, position.y)
+
+	print(position)
+	print(from)
+
+	$Tween.interpolate_property(self, "position", from, to, 2, Tween.EASE_IN, Tween.EASE_OUT)
+
+	$Tween.start()
+
+
+func _on_Tween_completed(object: Object, key: NodePath) -> void:
+	index += 1
+	if index > BACKWARD:
+		index = FORWARD
+
+	match index:
+		FORWARD:
+			$Tween.interpolate_property(
+				self, "position", from, to, DURATION, Tween.EASE_IN, Tween.EASE_OUT
+			)
+		BACKWARD:
+			$Tween.interpolate_property(
+				self, "position", to, from, DURATION, Tween.EASE_IN, Tween.EASE_OUT
+			)
+
+	$Tween.start()
