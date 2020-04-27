29b8195e9d46943e84f23359a8deea338b779a25
unknown <lucf.langis@gmail.com>
2020-04-27 10:49:27 +0200

    feat(interface): add a success notification when all letter are found

--- characters/player/ui/LettersIndicator.gd
+++ characters/player/ui/LettersIndicator.gd
@@ -3,11 +3,12 @@ extends Control
 
 func _ready() -> void:
 	GameManager.connect("letter_found", self, "_on_Letter_changed")
+	GameManager.connect("all_letters_found", $AnimationPlayer, "play", ["Success"])
 
 
 # signal letter_found - GameManager
 func _on_Letter_changed(letter_found: String) -> void:
 	print("letter_found: %s" % [letter_found])
-	for letter in $HBoxContainer.get_children():
-		if letter.text == letter_found:
+	for letter in get_children():
+		if letter is Label and letter.text == letter_found:
 			letter.modulate = Color(1, 1, 1, 1)
