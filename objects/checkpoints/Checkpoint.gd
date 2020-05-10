470582a3e6e119a45f73a1d9c0ff6279b43b6060
unknown <lucf.langis@gmail.com>
2020-05-10 11:46:04 +0200

    refacto(checkpoint): remove old behavior

--- objects/checkpoints/Checkpoint.gd
+++ objects/checkpoints/Checkpoint.gd
@@ -4,7 +4,6 @@ class_name Checkpoint
 
 
 func _ready():
-	$AnimationPlayer.connect("animation_finished", self, "_on_Animation_finished")
 	$AnimationPlayer.play("Inactive")
 	$Area2D.connect("body_entered", self, "_on_Player_enter")
 
@@ -13,13 +12,6 @@ func _ready():
 # @param {Player} body
 func _on_Player_enter(body: Player) -> void:
 	assert(body is Player)
-	if $AnimationPlayer.current_animation == "Inactive":
-		$AnimationPlayer.play("Transition")
-		GameManager.set_new_checkpoint(position)
-
-
-# @signal animation_finished
-# @param {String} anim_name
-func _on_Animation_finished(anim_name: String) -> void:
-	if anim_name == "Transition":
-		$AnimationPlayer.play("Activated")
+	$Area2D.disconnect("body_entered", self, "_on_Player_enter")
+	$AnimationPlayer.play("Transition")
+	GameManager.set_new_checkpoint(position)
